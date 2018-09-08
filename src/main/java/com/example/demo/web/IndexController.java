26fc408d9018a786d020ed6210d77b9ff48764b2
moon35710 <w819210252>
2018-09-08 13:44:53 +0800

    add

--- src/main/java/com/example/demo/web/IndexController.java
+++ src/main/java/com/example/demo/web/IndexController.java
@@ -2,6 +2,7 @@ package com.example.demo.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
@@ -9,5 +10,10 @@ public class IndexController {
     @RequestMapping("/")
     public  String index(){
         return  "index";
+    }
+    @ResponseBody
+    @RequestMapping("/check")
+    public  String check(){
+        return  "success";
     }
 }
